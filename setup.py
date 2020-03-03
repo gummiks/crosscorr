f8c400f4fd262d611541c7054632b3842592dc85
Ryan Terrien <rterrien@spiff.physics.carleton.edu>
2020-03-03 17:28:24 -0600

    adjusting for installation on spiff

--- setup.py
+++ setup.py
@@ -7,11 +7,17 @@ Created on Sun Nov 17 17:13:04 2019
 """
 
 import setuptools
+from numpy.distutils.core import setup, Extension
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+lib1 = Extension(name='ccf.CCF_1d',sources=['src/CCF_1d.f'])
+lib2 = Extension(name='ccf.CCF_3d',sources=['src/CCF_3d.f'])
+lib3 = Extension(name='ccf.CCF_pix',sources=['src/CCF_pix.f'])
+
+
+setup(
     name="ccf", # Replace with your own username
     version="0.0.1",
     author="Ryan Terrien",
@@ -20,7 +26,13 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
 #    url="https://github.com/pypa/sampleproject",
-    packages=setuptools.find_packages(),
+    #packages=setuptools.find_packages(where='./src'),
+    package_dir={"ccf":"src"},
+    packages=setuptools.find_packages(),#[''],#setuptools.find_packages(),
+    py_modules=['ccf.mask','ccf.utils','ccf.airtovac'],
+    #py_modules=['ccf.mask'],
+    #packages=['mask'],
+    ext_modules = [lib1,lib2,lib3],
     classifiers=[
         "Programming Language :: Python :: 2",
 #        "License :: OSI Approved :: MIT License",
--- src/mask.py
+++ src/mask.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import numpy as np
 import os
-import airtovac
+import ccf.airtovac
 import astropy.constants as aconst
 
 HARPSMASKDIR = os.path.join(os.path.dirname(__file__),"..","data","harps","masks")
@@ -12,34 +12,33 @@ class Mask:
     def __init__(self,filename=G2MASK,constant_v=True,disp=2.,use_airtovac=False):
         """
         A simple mask class.
-
         Reads in a .mas file that has the following columns:
          left    right     weights
         
-	INPUT:
-	    filename - mask filename
+    INPUT:
+        filename - mask filename
             constant_v - use a constant velocity width (RECOMMENDED)
             dispersion - half-width of the constant velocity width in km/s
             use_airtovac: convert from airwavelength to vacuum. 
                           Only for HARPS, as the HPF masks are in vacuum 
                           wavelength already.
 
-	OUTPUT:
-	    No output, main parameters of the object are:
+    OUTPUT:
+        No output, main parameters of the object are:
             self.wi - left-edge wavelengths of mask
             self.wf - right-edge wavelegnths of mask
             self.weight - mask weights
 
-	EXAMPLE:
+    EXAMPLE:
             M = mask.Mask(filename="../data/hpf/masks/gj699.mas",
-			  disp=2.,constant_v=True)
-	    
+              disp=2.,constant_v=True)
+        
         """
         print("AIRTOVAC (TRUE for HARPS, FALSE TRUE HPF): ",use_airtovac)
         self.wi, self.wf, self.weight = np.loadtxt(filename,unpack=True,dtype=np.float64)
-	if use_airtovac:
-            self.wi = airtovac.airtovac(self.wi)
-            self.wf = airtovac.airtovac(self.wf)
+        if use_airtovac:
+            self.wi = ccf.airtovac.airtovac(self.wi)
+            self.wf = ccf.airtovac.airtovac(self.wf)
         self.wmid = 0.5*(self.wi+self.wf)
         width_in_km = self.wmid * disp / (aconst.c.value/1.e3)
         self.wi_v = self.wmid - width_in_km/2.
